c73b3088a15ab2df247a89bd99fe5656aca1dbe9
Syed Wajahat Ali <wajahat.klk25.com>
2023-02-24 00:22:23 +0100

    adds controlled inputs

--- src/components/GeneratorForm.jsx
+++ src/components/GeneratorForm.jsx
@@ -1,34 +1,54 @@
 import React, { useState } from "react";
-
-function handleChange(e) {
-  console.log(e.target.value);
-}
-
-function handleSubmit(a) {
-  console.log(a);
-}
-
+import memesData from "../memesData";
 export default function GeneratorForm() {
-  const [topText, setTopText] = useState("");
-  const [bottomText, setbottomText] = useState("");
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+  function handleChange(e) {
+    setMeme((prevMeme) => {
+      return {
+        ...prevMeme,
+        [e.target.name]: e.target.value,
+      };
+    });
+  }
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    return setMeme((prevMeme) => ({ ...prevMeme, randomImage: url }));
+  }
   return (
-    <div className="form--container">
+    <div className="form--main">
       <h2 className="form--heading">Generate meme</h2>
-      <input
-        type="text"
-        placeholder="top text"
-        onChange={handleChange}
-        className="input"
-      />
-      <input
-        type="text"
-        placeholder="bottom text"
-        className="input"
-        onChange={handleChange}
-      />
-      <button className="form--btn" onClick={handleSubmit}>
-        Generate Meme
+      <div>
+        <input
+          type="text"
+          placeholder="top text"
+          onChange={handleChange}
+          className="input"
+          name="topText"
+          value={meme.topText}
+        />
+        <input
+          type="text"
+          placeholder="bottom text"
+          className="input"
+          onChange={handleChange}
+          name="bottomText"
+          value={meme.bottomText}
+        />
+      </div>
+      <button className="form--btn" onClick={getMemeImage}>
+        New MEME Image
       </button>
+      <div>
+        <p className="text--top">{meme.topText}</p>
+        <img src={meme.randomImage} alt={meme.randomImage} className="meme" />
+        <p className="text--bottom">{meme.bottomText}</p>
+      </div>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import memeImg from "../images/meme.png";
+import memeFace from "../images/meme-face.png";
 
 export default function Header() {
   return (
     <div className="header--container">
       <img src={memeImg} alt="meme img" className="meme--img" />
       <h2 className="heading">Meme Generator</h2>
-      <h3 className="heading--line">Syed Wajahat</h3>
+      <img src={memeFace} alt="meme face" className="meme--img" />
     </div>
   );
 }
